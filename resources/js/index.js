986f704649869da9d761de827fa0ad1e877a754a
AndreasDEV <andreasmrdja16@gmail.com>
2019-01-19 22:34:31 +0100

    Revert "Started working on header and intro section. RFI. | Update #2 |"
    This reverts commit 82a6429c563f36f9f2e2e0bba7e4ae03ccec19a6.

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,13 +1,2 @@
 // * --- WEBPACK IMPORT FILES --- * \\
 import '../sass/main.scss';
-
-// * --- ScrollOut Implementation --- * \\
-console.clear();
-ScrollOut({
-  cssProps: {
-    visibleY: true,
-    viewportY: true
-  }
-});
-
-Splitting({ target: '.heading' });
