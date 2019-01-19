bd4ae27342adaa4dc7a3fa34c70e86c907f2bc56
AndreasDEV <andreasmrdja16@gmail.com>
2019-01-19 22:50:54 +0100

    Finished working on header and intro section. | Update #2 |  LastUpdate was reverted!

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,2 +1,14 @@
 // * --- WEBPACK IMPORT FILES --- * \\
 import '../sass/main.scss';
+
+// * --- ScrollOut Implementation --- * \\
+console.clear();
+
+ScrollOut({
+  cssProps: {
+    visibleY: true,
+    viewportY: true
+  }
+});
+
+Splitting({ target: '.heading' });
